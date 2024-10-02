003859e1cca6a4d134f4d64b4641a91d76658650
Seungmin Cho <sungm97@gmail.com>
2024-10-02 14:28:22 -0500

    Everything working as intended

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 
-int validation(int lower, int upper, const string& prompt = "Please enter a value: ", const string& error = "Your value is invalid")
+int validation(int lower = numeric_limits<int>::min(), int upper= numeric_limits<int>::max(), const string& prompt = "Please enter a value: ", const string& error = "Your value is invalid")
 {
     int input;
     bool check = false;
@@ -25,7 +26,13 @@ int validation(int lower, int upper, const string& prompt = "Please enter a valu
 int main()
 {
     int test;
-    test = validation(0, 100);
-    cout << "You entered: " << test;
+    test = validation();
+    cout << "You entered: " << test << endl;
+    test = validation(0,100,"Please enter a value between 0 and 100");
+    cout << "You entered: " << test << endl;
+    test = validation(0,numeric_limits<int>::max(), "Please enter a value greater or equal to 0");
+    cout << "You entered: " << test << endl;
+    test = validation(numeric_limits<int>::min(),100, "Please enter a value less or equal to 100");
+    cout << "You entered: " << test << endl;
     return 0;
 }
